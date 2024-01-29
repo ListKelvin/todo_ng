ee3e8502088908e0999737b5ecabeaf755bc7523
Phạm Công Minh <phamm5687@gmail.com>
2024-01-30 00:33:24 +0700

    chore: create task template and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HelloWorldComponent } from './hello-world.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
