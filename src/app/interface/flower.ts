04cf2fad1a653fabaeda1016764117dfb41a0e22
Phạm Công Minh <phamm5687@gmail.com>
2024-02-16 23:18:48 +0700

    chore: setup model

---
+++ src/app/interface/flower.ts
@@ -0,0 +1,11 @@
+export interface Flower {
+  id: number;
+  name: string;
+  rating: number;
+  price: number;
+  isTopOfTheWeek: boolean;
+  image: string;
+  color: string;
+  origin: string;
+  category: string;
+}
--- src/app/interface/housinglocation.ts
+++ src/app/interface/housinglocation.ts
@@ -8,15 +8,3 @@ export interface HousingLocation {
   wifi: boolean;
   laundry: boolean;
 }
-
-export interface Flower {
-  id: number;
-  name: string;
-  rating: number;
-  price: number;
-  isTopOfTheWeek: boolean;
-  image: string;
-  color: string;
-  origin: string;
-  category: string;
-}
